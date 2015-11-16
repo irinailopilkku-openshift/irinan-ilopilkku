f580fd4ae848c6379f4e0861943b68add18c3a6d
LordMike <lordmike@iki.fi>
2015-11-16 21:29:33 +0200

    menu tesmaus

--- hermoratahierontaa.php
+++ hermoratahierontaa.php
@@ -21,13 +21,13 @@
 				<button class="dl-trigger">Open Menu</button>
 				<ul class="dl-menu">
 					<li><a href="index.php">Etusivu</a></li>
-					<li><a href="index.php">Tietoa minusta</a></li>
-					<li><a href="index.php">Palvelut</a></li>
+					<!-- <li><a href="index.php">Tietoa minusta</a></li> -->
+					<!-- <li><a href="index.php">Palvelut</a></li> -->
 					<!-- <li><a href="index.php">Yhteystiedot</a></li> -->
-					<li><a href="index.php#contact">Yhteystiedot</a></li>
-					<li><a href="http://www.irinanilopilkku.fi/index.php#contact">Yhteystiedot</a></li>
+					<!-- <li><a href="index.php#contact">Yhteystiedot</a></li> -->
+					<!-- <li><a href="http://www.irinanilopilkku.fi/index.php#contact">Yhteystiedot</a></li> -->
 					<!-- <li><a href="#works">Works</a></li> -->
-					<li><a href="index.php">Kuvia</a></li>
+					<!-- <li><a href="index.php">Kuvia</a></li> -->
 					<li>
 						<a href="#">Sivut</a>
 						<ul class="dl-submenu">
